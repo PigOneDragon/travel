6a243beb1e24506635a71f7cd34ff74666fc8d43
PigOneDragon <18137410276@189.cn>
2019-07-11 00:38:42 +0800

    城市页面路由

---
+++ src/pages/city/city.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="city">
+      <city-header></city-header>
+    </div>
+</template>
+<script>
+import CityHeader from './components/cityheader'
+export default {
+  name: 'city',
+  components: {
+    CityHeader
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/pages/city/components/cityheader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="city">
+        <div class="header-city">
+            <router-link to="/">
+                <span class="iconfont cityHeadBack">&#xe624;</span>
+            </router-link>
+            城市查询
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'cityheader'
+}
+</script>
+
+<style scoped>
+    .header-city{
+        height: .86rem;
+        line-height: .86rem;
+        background-color: #00bcd4;
+        padding: 0 .2rem;
+        text-align: center;
+        color: #fff;
+        font-size: .32rem;
+    }
+    .cityHeadBack{
+        float: left;
+        color: #fff;
+    }
+</style>
